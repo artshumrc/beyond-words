c279939b0040f163723c3b39b24ee036b4a02fde
Julian Kingman <julian@designman.io>
2016-07-26 18:55:05 -0400

    replaced react-layout with react-mounter, per Kadiras recommendation (see https://github.com/kadirahq/react-mounter)

---
+++ collections/books.js
@@ -0,0 +1,57 @@
+this.Books = new Meteor.Collection('books');
+
+Schemas.Books = new SimpleSchema({
+    title: {
+        type: String,
+        max: 60
+    },
+    createdAt: {
+        type: Date,
+        autoValue: function () {
+            if (this.isInsert) {
+                return new Date();
+            }
+        }
+    },
+    updatedAt: {
+        type: Date,
+        optional: true,
+        autoValue: function () {
+            if (this.isUpdate) {
+                return new Date();
+            }
+        }
+    },
+    title: {
+        type: String
+    },
+    dateBegun: {
+        type: Date
+    },
+    dateEnded: {
+        type: Date
+    },
+    author: {
+        type: String
+    },
+    illuminator: {
+        type: String
+    },
+    place: {
+        type: String
+    },
+    externalUrl: {
+        type: String
+    },
+    shelfmark: {
+        type: String
+    },
+    description: {
+        type: String,
+        autoform: {
+            rows: 5
+        }
+    }
+});
+
+Books.attachSchema(Schemas.Books);
