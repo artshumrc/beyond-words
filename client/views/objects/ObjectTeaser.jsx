4eff833082ebfcb9bd784f40ab1557a978fd69d5
Luke Hollis <lukehollis@gmail.com>
2016-08-08 03:31:41 -0400

    add teaser fields in wireframe and default teaser image

--- client/views/objects/ObjectTeaser.jsx
+++ client/views/objects/ObjectTeaser.jsx
@@ -1,12 +1,8 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import IconButton from 'material-ui/IconButton';
 import Divider from 'material-ui/Divider';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import CommunicationComment from 'material-ui/svg-icons/communication/comment';
-import ActionInput from 'material-ui/svg-icons/action/input';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 
 // Needed for onTouchTap
 // Check this repo:
@@ -31,31 +27,24 @@ ObjectTeaser = React.createClass({
 
      return (
        <div className="object-teaser">
-         <Card>
-            <a href={object_url}>
-              <CardTitle title={object.title} />
-            </a>
-            <CardText>
-            </CardText>
-            <Divider />
-            <CardActions>
-              <a href="#">
-                <IconButton tooltip="Comment">
-                  <CommunicationComment />
-                </IconButton>
-              </a>
-              <a href="#">
-                <IconButton tooltip="Favorite">
-                  <ActionFavoriteBorder />
-                </IconButton>
-              </a>
-              <a href="#">
-                <IconButton tooltip="Other Formats">
-                  <ActionInput />
-                </IconButton>
-              </a>
-            </CardActions>
-          </Card>
+				 <a href={object_url}>
+					 <div className="object-thumbnail-wrap">
+						 {object.thumbnail ?
+							 <img className="object-thumbnail" src={object.thumbnail} />
+						 : 
+							 <img className="object-thumbnail" src="/images/default_object.jpg" />
+						 }
+					 </div>
+				 </a>
+				 <div className="object-text-wrap">
+					 <a href={object_url}>
+						 <h3>{object.title}</h3>
+					 </a>
+					 <span className="object-teaser-subtitle">{object.author} Dates-Dates</span>
+					 <p>
+						 Example description quid faciat laetas segetes quo sidere terram vertere Mycenas ulmisque adiungere vites conveniat . . .
+					 </p>
+				 </div>
         </div>
       );
     }
