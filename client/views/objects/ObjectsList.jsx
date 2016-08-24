e5e81ce588cb607bf8429ab244c01098910cbab2
Suhaib Khan <suheb.work@gmail.com>
2016-08-24 19:26:53 +0530

    Fixing indentation and slick slider in ObjectList
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- client/views/objects/ObjectsList.jsx
+++ client/views/objects/ObjectsList.jsx
@@ -14,8 +14,8 @@ ObjectsList = React.createClass({
     mixins: [ReactMeteorData],
 
     propTypes: {
-				selectedObject: React.PropTypes.object,
-				selectObject: React.PropTypes.func,
+		selectedObject: React.PropTypes.object,
+		selectObject: React.PropTypes.func,
         filters: React.PropTypes.array,
         addSearchTerm: React.PropTypes.func,
         toggleSearchTerm: React.PropTypes.func,
@@ -30,20 +30,20 @@ ObjectsList = React.createClass({
         };
     },
 
-		objects: [],
+	objects: [],
 
-		getChildContext() {
-			return { muiTheme: getMuiTheme(baseTheme) };
-		},
-		childContextTypes: {
-	    muiTheme: React.PropTypes.object.isRequired,
-		},
+	getChildContext() {
+		return { muiTheme: getMuiTheme(baseTheme) };
+	},
+
+	childContextTypes: {
+        muiTheme: React.PropTypes.object.isRequired,
+	},
 
     getMeteorData(){
         var query = {},
-            objects = [],
-						stillMoreObjects = true;
-
+        objects = [],
+		stillMoreObjects = true;
 
         // Parse the filters to the query
         this.props.filters.forEach(function (filter) {
@@ -77,38 +77,35 @@ ObjectsList = React.createClass({
                     var date = moment("01/01/" + filter.values[0]);
                     query['dateEnded'] = {$lte: new Date(date.toISOString())};
                     break;
-
             }
         });
 
         console.log("Objects query:", query);
         var handle = Meteor.subscribe('objects', query, this.props.skip, this.props.limit);
         if (handle.ready()) {
-          objects = Objects.find({}, {}).fetch();
+            objects = Objects.find({}, {}).fetch();
+            objects.forEach(function(object){
+                var imageSubscription = Meteor.subscribe('objectImages', object.slug);
+                if (imageSubscription.ready()) {
+                    object.images = Images.find({}).fetch();
+                    object.thumbnails = Thumbnails.find({}).fetch();
+                }
+            });
 
-					objects.forEach(function(object){
-		        var imageSubscription = Meteor.subscribe('objectImages', object.slug);
-		        if (imageSubscription.ready()) {
-		            object.images = Images.find({}).fetch();
-		            object.thumbnails = Thumbnails.find({}).fetch();
-		        }
-
-					});
-
-					if(objects.length < this.props.limit){
-						stillMoreObjects = false;
-					}
-				}
+            if(objects.length < this.props.limit){
+                stillMoreObjects = false;
+            }
+        }
 
         return {
             objects: objects,
-						stillMoreObjects: stillMoreObjects
+			stillMoreObjects: stillMoreObjects
         };
     },
 
 
     componentDidMount(){
-			// this.hide();
+		// this.hide();
     },
 
     handleImagesLoaded: function(imagesLoadedInstance) {
@@ -116,37 +113,34 @@ ObjectsList = React.createClass({
     },
 
     renderObjects(isInSlider) {
-			var self = this;
+		var self = this;
 
-			if(!isInSlider){
-				isInSlider = false;
-			}
+		if(!isInSlider){
+			isInSlider = false;
+		}
 
-			if(
-					this.objects.length === 0
-				|| this.props.skip === 0
-			){
-				this.objects = this.data.objects;
+		if(this.objects.length === 0 || this.props.skip === 0) {
+			this.objects = this.data.objects;
+		}else {
+			//$("html, body").animate({ scrollTop: 0 }, "fast");
+			this.data.objects.forEach(function(object){
+				if(!self.objects.some(function(existingObject){
+					return existingObject._id === object._id
+				})){
+					self.objects.push(object);
+				}
 
-			}else {
-				//$("html, body").animate({ scrollTop: 0 }, "fast");
-				this.data.objects.forEach(function(object){
-					if(!self.objects.some(function(existingObject){
-						return existingObject._id === object._id
-					})){
-						self.objects.push(object);
-					}
-
-				});
-			}
-
-			console.log("render object", this.objects.length);
-      return this.objects.map((object) => {
-          return <ObjectTeaser
-              key={object._id}
-              object={object}
-							selectObject={self.props.selectObject}
-							/>;
+			});
+		}
+		console.log("render object", this.objects.length);
+        return this.objects.map((object) => {
+            return (
+                <ObjectTeaser
+                    key={object._id}
+                    object={object}
+                    selectObject={self.props.selectObject}
+                />
+            )
       });
     },
 
@@ -180,94 +174,116 @@ ObjectsList = React.createClass({
 
         let self = this;
 
-		    let masonryOptions = {
-		      //columnWidth : "400px",
-		      //isFitWidth : true,
-		      transitionDuration : 300
-		    };
+	    let masonryOptions = {
+            //columnWidth : "400px",
+            //isFitWidth : true,
+            transitionDuration : 300
+	    };
 
-				let selectedObject = this.props.selectedObject;
+		let selectedObject = this.props.selectedObject;
 
-		    const settings = {
-		      //focusOnSelect: true,
-		      dots: true,
-		      //infinite: true,
-		      //slidesToShow: 4,
-		      slidesToScroll: 1,
-		    };
+	    const settings = {
+            focusOnSelect: true,
+            dots: true,
+            //infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+            responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+            ]
+	    };
 
         return (
             <div className="objects-list wow fadeIn">
 
-							<FiltersWidget
-								filters={this.props.filters}
-								toggleSearchTerm={this.props.toggleSearchTerm}
-								/>
+				<FiltersWidget
+					filters={this.props.filters}
+					toggleSearchTerm={this.props.toggleSearchTerm}
+                    />
 
-							{("author_title" in selectedObject && selectedObject.author_title.length) ?
-								<div>
-									<ObjectDetail
-										object={this.props.selectedObject}
-										closeSelectedObject={this.props.closeSelectedObject}
-										/>
+				{("author_title" in selectedObject && selectedObject.author_title.length) ?
+					<div>
+						<ObjectDetail
+							object={this.props.selectedObject}
+							closeSelectedObject={this.props.closeSelectedObject}
+						/>
 
-									<div className="row">
-										<div className="col-xs-11 center-block clear">
-											<Slider
-												className="objects-list-slider"
-												{...settings}>
-									      {this.objects.map((object) => {
-									          return <ObjectTeaser
-									              key={object._id}
-									              object={object}
-																selectObject={self.props.selectObject}
-																isInSlider={true}
-																/>;
-														})}
-											</Slider>
-										</div>
-									</div>
+						<div className="row">
+							<div className="col-xs-11 center-block clear">
+								<Slider {...settings}>
+						            {this.objects.map((object, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <div className="object-slider-teaser">
+                                                <ObjectTeaser
+                                                    key={object._id}
+                                                    object={object}
+                                                    selectObject={self.props.selectObject}
+                                                    isInSlider={true}
+                                                />;
+                                                </div>
+                                            </div>
+                                        )
+								    })}
+								</Slider>
+							</div>
+						</div>
 
-								</div>
+					</div>
+					:
+					<div>
+						<InfiniteScroll
+							endPadding={120}
+							loadMore={debounce(1000, this.props.loadMoreObjects)} >
 
-								:
-								<div>
-									<InfiniteScroll
-										endPadding={120}
-										loadMore={debounce(1000, this.props.loadMoreObjects)}
-										>
+							{this.props.catalogLayout === "grid" ?
+                                <Masonry
+                                    options={masonryOptions}
+                                    className="objects-container objects-container--grid"
+                                    onImagesLoaded={this.handleImagesLoaded} >
 
-										{this.props.catalogLayout === "grid" ?
-									    <Masonry
-							          options={masonryOptions}
-							          className="objects-container objects-container--grid"
-				                onImagesLoaded={this.handleImagesLoaded}
-							          >
-			                    {this.renderObjects()}
-									    </Masonry>
-										: <div className="objects-container objects-container--list">
-			                    {this.renderObjects()}
-											</div>
-										}
-									</InfiniteScroll>
+                                    {this.renderObjects()}
 
-									{this.data.stillMoreObjects ?
-		                <div className="loading-collections loading-visible">
-											<div className="dot-spinner">
-												  <div className="bounce1"></div>
-												  <div className="bounce2"></div>
-												  <div className="bounce3"></div>
-												</div>
-		                </div>
-
-									: ""}
-									{this.objects.length === 0 && !this.data.stillMoreObjects ?
-										<div className="no-results no-results--objects">
-											<p>No manuscripts were found for your query.</p>
-										</div>
-									: "" }
-								</div>
+                                </Masonry>
+                                :
+                                <div className="objects-container objects-container--list">
+                                    {this.renderObjects()}
+                                </div>
 							}
+						</InfiniteScroll>
+
+						{this.data.stillMoreObjects ?
+                            <div className="loading-collections loading-visible">
+								<div className="dot-spinner">
+                                    <div className="bounce1"></div>
+                                    <div className="bounce2"></div>
+                                    <div className="bounce3"></div>
+                                </div>
+                            </div>
+                            : ""
+                        }
+						{this.objects.length === 0 && !this.data.stillMoreObjects ?
+							<div className="no-results no-results--objects">
+								<p>No manuscripts were found for your query.</p>
+							</div>
+                            : ""
+                        }
+					</div>
+				}
             </div>
         );
     }
